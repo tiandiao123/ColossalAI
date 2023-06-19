45558b4db9d6849f90f23757eada5d23d887512b
cuiqing.li <lixx3527@gmail.com>
2023-06-19 17:11:24 +0800

    add trainer

--- examples/images/stable-diffusion/text-to-images/run_trainer_no_collosal.sh
+++ examples/images/stable-diffusion/text-to-images/run_trainer_no_collosal.sh
@@ -11,7 +11,7 @@ accelerate launch --mixed_precision="fp16"  stable_diffusion_trainer.py \
   --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
   --gradient_checkpointing \
-  --max_train_steps=15000 \
+  --max_train_steps=150 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
