d42408ada2d1d7d021b68e66db4843205225ee28
cuiqing.li <lixx3527@gmail.com>
2023-06-25 13:39:44 +0800

    inference working on

--- applications/stable-diffusion/text-to-images/02_run_trainer_with_colossalai.sh
+++ applications/stable-diffusion/text-to-images/02_run_trainer_with_colossalai.sh
@@ -12,7 +12,7 @@ torchrun --nproc_per_node 4 stable_diffusion_colossalai_trainer.py \
     --train_batch_size=1 \
     --gradient_accumulation_steps=4 \
     --gradient_checkpointing \
-    --max_train_steps=800 \
+    --max_train_steps=15000 \
     --learning_rate=1e-05 \
     --max_grad_norm=1 \
     --lr_scheduler="constant" --lr_warmup_steps=0 \
